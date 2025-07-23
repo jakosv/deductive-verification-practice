b3cd5f3b002f4e752f96db7655a6c3e75363d068
Vadim Marchenko <jakosvadim@gmail.com>
2025-07-23 15:22:25 +0300

    Add correct files extension

--- why3/hw1/check.sh
+++ why3/hw1/check.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-why3 prove -P alt-egro hw1.why
-why3 prove -P z3 hw1.why
-why3 prove -P z3-ce hw1.why
-why3 prove -P z3-nobv hw1.why
+why3 prove -P alt-egro hw1.mlw
+why3 prove -P z3 hw1.mlw
+why3 prove -P z3-ce hw1.mlw
+why3 prove -P z3-nobv hw1.mlw
